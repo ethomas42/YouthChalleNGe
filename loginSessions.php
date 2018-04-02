778555d7d027362e0d6a9b40b9aacbd151095d41
Czineffect <35840623+Czineffect@users.noreply.github.com>
2018-04-02 19:31:59 -0400

    Add files via upload

--- loginSessions.php
+++ loginSessions.php
@@ -6,8 +6,8 @@
 require_once 'dbcontroller.php'; 
 session_start(); 
 $connection = new DBController(); 
-//$username = $_POST[]; 
-//$password = $_POST[]; 
+$username = filter_input(INPUT_POST, "inputUsername");
+$password = filter_input(INPUT_POST, "inputPassword"); 
 
 
 //$rowCount = $connection->numRows("SELECT * FROM users WHERE username = '$username' AND password = '$password'"); 
@@ -16,6 +16,7 @@ if($rowCount)
 {
      $record = $connection->runQuery("SELECT * FROM users WHERE 1")[0];
      $_SESSION["loggedIn"] = true; 
+     
      setcookie("username", $record['username'], time() + 86400, "/"); 
      header("Location: allCadetView.php?");
      
