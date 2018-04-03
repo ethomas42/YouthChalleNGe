4438270c6ac050e2f4b9ca5afd3ddae30a44cf11
CharliePRamsey <parkerrmsy@gmail.com>
2018-04-03 19:29:33 -0400

    report screen progress

--- allReportView.php
+++ allReportView.php
@@ -5,26 +5,33 @@
 ?>
 
 			<div class="container col-sm-12">
+				<button type="button" class="btn btn-danger">New</button>
+				
+				<p><br></p>
+				
 				<table id="reports-table" class="table table-striped table-bordered" cellspacing="0">
 					<thead>
 						<tr>
 							<th>View</th>
 							<th>Name</th>
+							<th>Date Created</th>
 						</tr>
 					</thead>
 					<tbody>
 						<?php
 							include_once "dbcontroller.php";
 							$db = new DBController();
-							if ($db->numRows("SELECT name FROM reports"))
+							if ($db->numRows("SELECT name, dateCreated FROM reports"))
 							{
-								$results = $db->runQuery("SELECT name FROM reports");
+								$results = $db->runQuery("SELECT name, dateCreated FROM reports");
 								foreach($results as $row)
 								{
+									$dateA = preg_split('/[-_ ]+/', $row['dateCreated']); //split by dash underscore and space
 									echo <<<_END
 											<tr>
 												<td><form method="post" action="reportView.php?{$row['name']}"><input type="hidden" value="0"><button type="button" class="btn btn-primary">View Report</button></form></td>
 												<td>{$row['name']}</td>
+												<td>{$dateA[1]}/{$dateA[2]}/{$dateA[0]}</td> <!-- American date format -->
 											</tr>
 _END;
 								}
@@ -36,30 +43,18 @@ _END;
 			
 			<!-- These div's make the page display correctly -->
 			
-			<div class='col-sm-1'>
-			</div>
-			<div class='col-sm-1'>
-			</div>
-			<div class='col-sm-1'>
-			</div>
-			<div class='col-sm-1'>
-			</div>
-			<div class='col-sm-1'>
-			</div>
-			<div class='col-sm-1'>
-			</div>
-			<div class='col-sm-1'>
-			</div>
-			<div class='col-sm-1'>
-			</div>
-			<div class='col-sm-1'>
-			</div>
-			<div class='col-sm-1'>
-			</div>
-			<div class='col-sm-1'>
-			</div>
-			<div class='col-sm-1'>
-			</div>
+			<div class='col-sm-1'></div>
+			<div class='col-sm-1'></div>
+			<div class='col-sm-1'></div>
+			<div class='col-sm-1'></div>
+			<div class='col-sm-1'></div>
+			<div class='col-sm-1'></div>
+			<div class='col-sm-1'></div>
+			<div class='col-sm-1'></div>
+			<div class='col-sm-1'></div>
+			<div class='col-sm-1'></div>
+			<div class='col-sm-1'></div>
+			<div class='col-sm-1'></div>
 			
 				<!-- beginning of end of basic page -->
             </div>
--- reportView.php
+++ reportView.php
@@ -6,8 +6,18 @@
 			
             <script>
             $(document).ready(function(){
-                $("#roleSettings").hide();
-                $('#inputRole').on('change', function() {
+                $("#allergyColumns").hide();
+				$("#attachmentColumns").hide();
+				$("#applicantColumns").hide();
+				$("#courtAssignmentColumns").hide();
+				$("#guardianColumns").hide();
+				$("#immunizationColumns").hide();
+				$("#insuranceColumns").hide();
+				$("#medicationColumns").hide();
+				$("#phoneNumberColumns").hide();
+				$("#referralColumns").hide();
+				$("#substanceAbuseColumns").hide();
+                $('#inputTable').on('change', function() {
                   if ( this.value == '1')
                   {
                     $("#roleSettings").show();
@@ -36,11 +46,208 @@
 						<div class="form-group col-sm-12">
 							<label for="inputTable">Table Name</label>
 							<select class="form-control" id="inputTable">
-								<option selected value="0">Cadets</option>
-								<option value="0">Applicants</option>
-								<option value="0">Users</option>
+								<option selected value="1">Cadets</option>
+								<option value="2">Applicants</option>
+								<option value="3">Users</option>
 							</select>
 						</div>
+						<table id="allergyColumns" class="table table-striped table-bordered" cellspacing="0">
+							<thead>
+								<tr>
+									<th>Allergy Columns</th>
+								</tr>
+							</thead>
+							<tbody>
+								<tr><td id='allergies.type'>Allergy Type</td></tr>
+								<tr><td id='allergies.notes'>Allergy Notes<td></tr>
+							</tbody>
+						</table>
+						<table id="attachmentColumns" class="table table-striped table-bordered" cellspacing="0">
+							<thead>
+								<tr>
+									<th>Attachment Columns</th>
+								</tr>
+							</thead>
+							<tbody>
+								<tr><td id='attachments.filename'>Filename</td></tr>
+								<tr><td id='attachments.uploadDate'>Upload Date<td></tr>
+							</tbody>
+						</table>
+						<table id="cadetColumns" class="table table-striped table-bordered" cellspacing="0">
+							<thead>
+								<tr>
+									<th>Cadets Columns</th>
+								</tr>
+							</thead>
+							<tbody>
+								<tr><td id='cadets.fName'>First Name</td></tr>
+								<tr><td id='cadets.mName'>Middle Name</td></tr>
+								<tr><td id='cadets.lName'>Last Name</td></tr>
+								<tr><td id='cadets.gender'>Gender</td></tr>
+								<tr><td id='cadets.ssn'>Social Security Number</td></tr>
+								<tr><td id='cadets.genQual'>Generational Qualifier</td></tr>
+								<tr><td id='cadets.birthday'>Date of Birth</td></tr>
+								<tr><td id='cadets.race'>Race</td></tr>
+								<tr><td id='cadets.isHispanic'>Hispanic</td></tr>
+								<tr><td id='cadets.email'>Email</td></tr>
+								<tr><td id='cadets.mStreet'>Mailing Address</td></tr>
+								<tr><td id='cadets.mStreet2'>Mailing Address Line 2</td></tr>
+								<tr><td id='cadets.mCity'>Mailing City</td></tr>
+								<tr><td id='cadets.mState'>Mailing State</td></tr>
+								<tr><td id='cadets.pStreet'>Physical Address</td></tr>
+								<tr><td id='cadets.pStreet2'>Physical Address Line 2</td></tr>
+								<tr><td id='cadets.pCity'>Physical City</td></tr>
+								<tr><td id='cadets.pState'>Physical State</td></tr>
+								<tr><td id='cadets.pZip'>Physical Zip</td></tr>
+								<tr><td id='cadets.isCitizen'>Citizenship</td></tr>
+								<tr><td id='cadets.ged'>GED</td></tr>
+								<tr><td id='cadets.volunteer'>Volunteer Status</td></tr>
+								<tr><td id='cadets.admissionStatus'>Admission Status</td></tr>
+								<tr><td id='cadets.schoolWithdrawDate'>School Withdrawal Date</td></tr>
+								<tr><td id='cadets.unemplyed'>Unemployed</td></tr>
+								<tr><td id='cadets.underemployed'>Underemployed</td></tr>
+								<tr><td id='cadets.workplace'>Place of Work</td></tr>
+								<tr><td id='cadets.wage'>Wage</td></tr>
+								<tr><td id='cadets.hoursWorking'>Hours per Week</td></tr>
+								<tr><td id='cadets.recBy'>Recommender</td></tr>
+								<tr><td id='cadets.gradeCompleted'>Grade Completed</td></tr>
+								<tr><td id='cadets.hairColor'>Hair Color</td></tr>
+								<tr><td id='cadets.eyeColor'>Eye Color</td></tr>
+								<tr><td id='cadets.height'>Height</td></tr>
+								<tr><td id='cadets.weight'>Weight</td></tr>
+								<tr><td id='cadets.personsInHouse'># of People in Household</td></tr>
+								<tr><td id='cadets.houseIncome'>Household Income</td></tr>
+								<tr><td id='cadets.gaResident'>GA Resident</td></tr>
+								<tr><td id='cadets.preferredComm'>Preferred Method of Communication</td></tr>
+							</tbody>
+						</table>
+						<table id="applicantColumns" class="table table-striped table-bordered" cellspacing="0">
+							<thead>
+								<tr>
+									<th>Applicant Columns</th>
+								</tr>
+							</thead>
+							<tbody>
+								<tr><td>First Name</td></tr>
+								<tr><td>Middle Name</td></tr>
+								<tr><td>Last Name</td></tr>
+								<tr><td>Gender</td></tr>
+								<tr><td>Social Security Number</td></tr>
+								<tr><td>Generational Qualifier</td></tr>
+								<tr><td>Date of Birth</td></tr>
+								<tr><td>Race</td></tr>
+								<tr><td>Hispanic</td></tr>
+								<tr><td>Email</td></tr>
+								<tr><td>Mailing Address</td></tr>
+								<tr><td>Mailing Address Line 2</td></tr>
+								<tr><td>Mailing City</td></tr>
+								<tr><td>Mailing State</td></tr>
+								<tr><td>Physical Address</td></tr>
+								<tr><td>Physical Address Line 2</td></tr>
+								<tr><td>Physical City</td></tr>
+								<tr><td>Physical State</td></tr>
+								<tr><td>Physical Zip</td></tr>
+								<tr><td>Citizenship</td></tr>
+								<tr><td>GED</td></tr>
+								<tr><td>Volunteer Status</td></tr>
+								<tr><td>Admission Status</td></tr>
+								<tr><td>School Withdrawal Date</td></tr>
+								<tr><td>Unemployed</td></tr>
+								<tr><td>Underemployed</td></tr>
+								<tr><td>Place of Work</td></tr>
+								<tr><td>Wage</td></tr>
+								<tr><td>Hours per Week</td></tr>
+								<tr><td>Accomplishment 1</td></tr>
+								<tr><td>Accomplishment 2</td></tr>
+								<tr><td>Recommender</td></tr>
+								<tr><td>Grade Completed</td></tr>
+								<tr><td>Hair Color</td></tr>
+								<tr><td>Eye Color</td></tr>
+								<tr><td>Height</td></tr>
+								<tr><td>Weight</td></tr>
+								<tr><td># of People in Household</td></tr>
+								<tr><td>Household Income</td></tr>
+								<tr><td>GA Resident</td></tr>
+								<tr><td>Preferred Method of Communication</td></tr>
+							</tbody>
+						</table>
+						<table id="courtAssignmentColumns" class="table table-striped table-bordered" cellspacing="0">
+							<thead>
+								<tr>
+									<th>Court Assignment Columns</th>
+								</tr>
+							</thead>
+							<tbody>
+								<tr><td>Allergy Type</td></tr>
+								<tr><td>Allergy Notes<td></tr>
+							</tbody>
+						</table>
+						<table id="allergyColumns" class="table table-striped table-bordered" cellspacing="0">
+							<thead>
+								<tr>
+									<th>Allergy Columns</th>
+								</tr>
+							</thead>
+							<tbody>
+								<tr><td>Allergy Type</td></tr>
+								<tr><td>Allergy Notes<td></tr>
+							</tbody>
+						</table>
+						<table id="allergyColumns" class="table table-striped table-bordered" cellspacing="0">
+							<thead>
+								<tr>
+									<th>Allergy Columns</th>
+								</tr>
+							</thead>
+							<tbody>
+								<tr><td>Allergy Type</td></tr>
+								<tr><td>Allergy Notes<td></tr>
+							</tbody>
+						</table>
+						<table id="allergyColumns" class="table table-striped table-bordered" cellspacing="0">
+							<thead>
+								<tr>
+									<th>Allergy Columns</th>
+								</tr>
+							</thead>
+							<tbody>
+								<tr><td>Allergy Type</td></tr>
+								<tr><td>Allergy Notes<td></tr>
+							</tbody>
+						</table>
+						<table id="allergyColumns" class="table table-striped table-bordered" cellspacing="0">
+							<thead>
+								<tr>
+									<th>Allergy Columns</th>
+								</tr>
+							</thead>
+							<tbody>
+								<tr><td>Allergy Type</td></tr>
+								<tr><td>Allergy Notes<td></tr>
+							</tbody>
+						</table>
+						<table id="allergyColumns" class="table table-striped table-bordered" cellspacing="0">
+							<thead>
+								<tr>
+									<th>Allergy Columns</th>
+								</tr>
+							</thead>
+							<tbody>
+								<tr><td>Allergy Type</td></tr>
+								<tr><td>Allergy Notes<td></tr>
+							</tbody>
+						</table>
+						<table id="allergyColumns" class="table table-striped table-bordered" cellspacing="0">
+							<thead>
+								<tr>
+									<th>Allergy Columns</th>
+								</tr>
+							</thead>
+							<tbody>
+								<tr><td>Allergy Type</td></tr>
+								<tr><td>Allergy Notes<td></tr>
+							</tbody>
+						</table>
 						<div class="form-group col-sm-12">
 							<button type='button' class='btn btn-success'>Add Field</button>
 						</div>
