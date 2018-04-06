30c517dc8aca7fe643a2ea7e8b9928613ea2cb5d
Czineffect <35840623+Czineffect@users.noreply.github.com>
2018-04-06 10:09:00 -0400

    Add files via upload
    Partially completed

---
+++ update-cadets.php
@@ -0,0 +1,114 @@
+<?php
+require_once 'phpFunctions.php'; 
+require_once 'dbcontroller.php';
+$connection = new DBController(); 
+if(isset($_POST['saveCadet']))
+{
+    
+    $key = $_POST['ssnKey']; 
+    if(isset($_POST['attachments']))
+    { 
+        importFile("cadet","saveCadet","attachment", $_POST['ssnKey']);
+    }
+    
+    if(isset($_POST['inputCommMethod']))
+    {
+        $preferredCommunication = filter_input(INPUT_POST, "inputCommMethod"); 
+        $connection->runQuery("UPDATE cadets SET preferredComm = '$preferredCommunication'");
+    }
+    
+    if(isset($_POST['ssn']))
+    {
+        $ssn = $_POST['ssn']; 
+        $connection->runQuery("UPDATE cadets SET ssn = '$ssn' WHERE ssn = '$key'");
+    }
+    
+    if(isset($_POST['inputFirstName']))
+    {
+        $firstName = $_POST['inputFirstName']; 
+        $connection->runQuery("UPDATE cadets SET fName = '$firstName' WHERE ssn = '$key'"); 
+        
+    }
+    if(isset($_POST['inputMiddleName']))
+    {
+        $firstName = $_POST['inputMiddleName']; 
+        $connection->runQuery("UPDATE cadets SET mName = '$firstName' WHERE ssn = '$key'"); 
+        
+    }
+    if(isset($_POST['inputLastName']))
+    {
+        $firstName = $_POST['inputLastName']; 
+        $connection->runQuery("UPDATE cadets SET lName = '$firstName' WHERE ssn = '$key'"); 
+        
+    }
+    if(isset($_POST['inputGenQual']))
+    {
+        $firstName = $_POST['inputGenQual']; 
+        $connection->runQuery("UPDATE cadets SET genQual = '$firstName' WHERE ssn = '$key'"); 
+        
+    }
+    if(isset(filter_input(INPUT_POST, "inputHair")))
+    {
+        $hairColor = filter_input(INPUT_POST,"inputHair"); 
+        $connection->runQuery("UPDATE cadets SET hairColor = '$firstName' WHERE ssn = '$key'"); 
+    }
+    
+    if(isset(filter_input(INPUT_POST, "inputEye")))
+    {
+        $eyeColor = filter_input(INPUT_POST,"inputEye"); 
+        $connection->runQuery("UPDATE cadets SET eyeColor = '$eyeColor' WHERE ssn = '$key'"); 
+    }
+    
+    if(isset(filter_input(INPUT_POST, "inputWeight")))
+    {
+        $weight= filter_input(INPUT_POST,"inputWeight"); 
+        $connection->runQuery("UPDATE cadets SET weight = '$weight' WHERE ssn = '$key'"); 
+    }
+     if(isset(filter_input(INPUT_POST, "inputEye")))
+    {
+        $eyeColor = filter_input(INPUT_POST,"inputEye"); 
+        $connection->runQuery("UPDATE cadets SET eyeColor = '$eyeColor' WHERE ssn = '$key'"); 
+    }
+     if(isset(filter_input(INPUT_POST, "inputGender")))
+    {
+        $gender = filter_input(INPUT_POST,"inputGender"); 
+        $connection->runQuery("UPDATE cadets SET gender = '$gender' WHERE ssn = '$key'"); 
+    }
+    
+    if(isset(filter_input(INPUT_POST, "inputEmail")))
+    {
+        $email = filter_input(INPUT_POST,"inputEmail",FILTER_VALIDATE_EMAIL); 
+        $connection->runQuery("UPDATE cadets SET email = '$email' WHERE ssn = '$key'"); 
+    }
+    if(isset(filter_input(INPUT_POST, "inputAdmission")))
+    {
+        $admission = filter_input(INPUT_POST,"inputAdmission"); 
+        $connection->runQuery("UPDATE cadets SET admissionStatus = '$admission' WHERE ssn = '$key'"); 
+    }
+     if(isset(filter_input(INPUT_POST, "inputAdmission")))
+    {
+        $admission = filter_input(INPUT_POST,"inputAdmission"); 
+        $connection->runQuery("UPDATE cadets SET admissionStatus = '$admission' WHERE ssn = '$key'"); 
+    }
+    
+    if(isset(filter_input(INPUT_POST, "inputLocation")))
+    {
+        $location = filter_input(INPUT_POST, "inputLocation");
+        $connection->runQuery("UPDATE cadets set campusLocation = '$location' WHERE ssn = '$key'"); 
+    }
+    if(isset(filter_input(INPUT_POST, "inputGAResident")))
+    {
+        $gaResident = filter_input(INPUT_POST, "inputGAResident"); 
+        $connection->runQuery("UPDATE cadets set gaResident = '$gaResident' WHERE ssn = '$key'"); 
+        
+    }
+    if (isset(filter_input(INPUT_POST, ""))) {
+        
+    }
+    echo '<script> alert("Cadet has been Updated!"); </script>';
+    header("refresh:2;url=allCadetView.php"); 
+    
+    //echo "Button is pushed";
+    
+}
+?>
