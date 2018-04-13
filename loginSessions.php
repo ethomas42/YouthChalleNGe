aefbaf92574523471b0502ac19c8942fceb30455
Czineffect <35840623+Czineffect@users.noreply.github.com>
2018-04-13 09:21:08 -0400

    Update loginSessions.php

--- loginSessions.php
+++ loginSessions.php
@@ -14,7 +14,7 @@ $password = $_POST['password'];
 $rowCount = $connection->numRows("SELECT * FROM users WHERE username = '$username' AND password = '$password'");
 if($rowCount)
 {
-     $record = $connection->runQuery("SELECT * FROM users WHERE 1")[0];
+     $record = $connection->runQuery("SELECT * FROM users WHERE username = '$username' AND password = '$password'")[0];
      $_SESSION["loggedIn"] = true; 
      
      setcookie("username", $record['username'], time() + 86400, "/"); 
