5fe395f220151f1a6ee60bde1d85f3cbb97ea132
Czineffect <35840623+Czineffect@users.noreply.github.com>
2018-04-13 09:20:18 -0400

    Update loginSessions.php

--- loginSessions.php
+++ loginSessions.php
@@ -7,20 +7,18 @@ require_once 'dbcontroller.php';
 session_start(); 
 $connection = new DBController(); 
 $username = filter_input(INPUT_POST, "inputUsername");
-$password = filter_input(INPUT_POST, "inputPassword"); 
+$password = $_POST['password']; 
 
 
 //$rowCount = $connection->numRows("SELECT * FROM users WHERE username = '$username' AND password = '$password'"); 
-$rowCount = $connection->numRows("SELECT * FROM users WHERE 1");
+$rowCount = $connection->numRows("SELECT * FROM users WHERE username = '$username' AND password = '$password'");
 if($rowCount)
 {
      $record = $connection->runQuery("SELECT * FROM users WHERE 1")[0];
      $_SESSION["loggedIn"] = true; 
      
      setcookie("username", $record['username'], time() + 86400, "/"); 
-     header("Location: allCadetView.php?");
-     
-           
+     header("Location: allCadetView.php?");    
 }
 
 
