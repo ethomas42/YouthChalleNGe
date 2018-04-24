fb72c319819acad2042a2e2e89d95b0df5f71c4f
jkh604 <james.harrison@bobcats.gcsu.edu>
2018-04-24 18:35:33 -0400

    Changed 'Fort Stuart' to 'Fort Stewart'

--- basicPage.php
+++ basicPage.php
@@ -1,7 +1,6 @@
 <!-- Version 5: Implements functions to create a View Cadet Page and an Edit Cadet Page -->
 <!DOCTYPE html>
 <?php
-
 function basicPage($pageName) //Basic Page Layout with Navbar and Footer
 {
 	echo <<<_END
@@ -10,9 +9,7 @@ function basicPage($pageName) //Basic Page Layout with Navbar and Footer
 				<meta charset="utf-8">
 				<meta name="viewport" content="width=device-width, initial-scale=1.0">
 				<meta http-equiv="X-UA-Compatible" content="IE=edge">
-
 				<title>Georgia Youth ChalleNGe Program</title>
-
 				 <!-- Bootstrap CSS CDN -->
 				<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 				<!-- Our Custom CSS -->
@@ -38,7 +35,6 @@ function basicPage($pageName) //Basic Page Layout with Navbar and Footer
 						<div class="sidebar-header">
 							<a href="home.html"><img class="logo" name="logo" src="ngyc.png" style="width:200px;height:200px" border="0"></a>
 						</div>
-
 						<ul class="list-unstyled components">
 							<p>Georgia Youth ChalleNGe Program</p>
 							<li>
@@ -47,40 +43,36 @@ function basicPage($pageName) //Basic Page Layout with Navbar and Footer
 									<li><a href="allCadetView.php">All Locations</a></li>
 									<li><a href="#">Milledgeville</a></li>
 									<li><a href="#">Fort Gordon</a></li>
-									<li><a href="#">Fort Stuart</a></li>
+									<li><a href="#">Fort Stewart</a></li>
 								</ul>
 								<a href="#admissionSubmenu" data-toggle="collapse" aria-expanded="false">Admissions</a>
 								<ul class="collapse list-unstyled" id="admissionSubmenu">
 									<li><a href="allApplicantView.php">All Locations</a></li>
 									<li><a href="#">Milledgeville</a></li>
 									<li><a href="#">Fort Gordon</a></li>
-									<li><a href="#">Fort Stuart</a></li>
+									<li><a href="#">Fort Stewart</a></li>
 								</ul>
 								<a href="#reportSubmenu" data-toggle="collapse" aria-expanded="false">Reports</a>
 								<ul class="collapse list-unstyled" id="reportSubmenu">
 									<li><a href="allReportView.php">All Locations</a></li>
 									<li><a href="#">Milledgeville</a></li>
 									<li><a href="#">Fort Gordon</a></li>
-									<li><a href="#">Fort Stuart</a></li>
+									<li><a href="#">Fort Stewart</a></li>
 								</ul>
 								<a href="#adminSubmenu" data-toggle="collapse" aria-expanded="false">Administration</a>
 								<ul class="collapse list-unstyled" id="adminSubmenu">
 									<li><a href="roles.php">All Locations</a></li>
 									<li><a href="#">Milledgeville</a></li>
 									<li><a href="#">Fort Gordon</a></li>
-									<li><a href="#">Fort Stuart</a></li>
+									<li><a href="#">Fort Stewart</a></li>
 								</ul>
 							</li>
 						</ul>
-
 					</nav>
-
 					<!-- Page Content Holder -->
 					<div id="content">
-
 						<nav class="navbar navbar-default">
 							<div class="container-fluid">
-
 								<div class="navbar-header">
 									<button type="button" id="sidebarCollapse" class="btn btn-info navbar-btn">
 										<i class="glyphicon glyphicon-align-left"></i>
@@ -88,7 +80,6 @@ function basicPage($pageName) //Basic Page Layout with Navbar and Footer
 									</button>
 									<h1>$pageName</h1>
 								</div>
-
 								<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 									<ul class="nav navbar-nav navbar-right">
 										<!-- COMMENTING OUT 'Page's
