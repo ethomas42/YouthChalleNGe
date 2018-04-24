34b4d93bb6071c5aab5b0f53a753cd5cc3a293b0
CharliePRamsey <parkerrmsy@gmail.com>
2018-04-24 18:21:07 -0400

    added rest of cells

--- export-data.php
+++ export-data.php
@@ -10,7 +10,7 @@
                 $sheet = $spreadsheet->getActiveSheet();
                 $db = new DBController();
 
-                if ($db->numRows("SELECT ssn, fName, mName, lName, genQual, gender, ssn, birthday, race, isHispanic, email, mStreet, mStreet2, mCity, mState, mZip FROM cadets"))
+                if ($db->numRows("SELECT ssn, fName, mName, lName, genQual, gender, ssn, birthday, race, isHispanic, email, mStreet, mStreet2, mCity, mState, mZip, pStreet, pStreet2, pCity, pState, pZip, isCitizen, ged, volunteer, admissionStatus, schoolWithdrawDate, unemployed, underemployed, workplace, wage, hoursWorking, accomplish1, accomplish2, recBy, recNum,  gradeCompleted, hairColor, eyeColor, height, weight, personsInHouse, houseIncome, gaResident, preferredComm, campusLocation FROM cadets"))
                 {
                   $results = $db->runQuery("SELECT ssn, fName, mName, lName, genQual, gender, ssn, birthday, race, isHispanic, email, mStreet, mStreet2, mCity, mState, mZip FROM cadets");
                     $sheet->setCellValue('A1', 'fName');
@@ -46,6 +46,35 @@
 	                    $sheet->setCellValue('M' . $int, $row['mCity']);
 	                    $sheet->setCellValue('N' . $int, $row['mState']);
 	                    $sheet->setCellValue('O' . $int, $row['mZip']);
+						$sheet->setCellValue('P' . $int, $row['pStreet']);
+						$sheet->setCellValue('Q' . $int, $row['pStreet2']);
+						$sheet->setCellValue('R' . $int, $row['pCity']);
+						$sheet->setCellValue('S' . $int, $row['pState']);
+						$sheet->setCellValue('T' . $int, $row['pZip']);
+						$sheet->setCellValue('U' . $int, $row['isCitizen']);
+						$sheet->setCellValue('V' . $int, $row['ged']);
+						$sheet->setCellValue('W' . $int, $row['volunteer']);
+						$sheet->setCellValue('X' . $int, $row['admissionStatus']);
+						$sheet->setCellValue('Y' . $int, $row['schoolWithdrawDate']);
+						$sheet->setCellValue('Z' . $int, $row['unemployed']);
+						$sheet->setCellValue('AA' . $int, $row['underemployed']);
+						$sheet->setCellValue('AB' . $int, $row['workplace']);
+						$sheet->setCellValue('AC' . $int, $row['wage']);
+						$sheet->setCellValue('AD' . $int, $row['hoursWorking']);
+						$sheet->setCellValue('AP' . $int, $row['accomplish1']);
+						$sheet->setCellValue('AF' . $int, $row['accomplish2']);
+						$sheet->setCellValue('AG' . $int, $row['recBy']);
+						$sheet->setCellValue('AH' . $int, $row['recNum']);
+						$sheet->setCellValue('AI' . $int, $row['  gradeCompleted']);
+						$sheet->setCellValue('AJ' . $int, $row['hairColor']);
+						$sheet->setCellValue('AK' . $int, $row['eyeColor']);
+						$sheet->setCellValue('AL' . $int, $row['height']);
+						$sheet->setCellValue('AM' . $int, $row['weight']);
+						$sheet->setCellValue('AN' . $int, $row['personsInHouse']);
+						$sheet->setCellValue('AO' . $int, $row['houseIncome']);
+						$sheet->setCellValue('AP' . $int, $row['gaResident']);
+						$sheet->setCellValue('AQ' . $int, $row['preferredComm']);
+						$sheet->setCellValue('AR' . $int, $row['campusLocation']);
 
 
 	                    $writer = new Xlsx($spreadsheet);
