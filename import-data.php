658a38d7a6239a4448d6df016d9de42d5c6ea709
Czineffect <35840623+Czineffect@users.noreply.github.com>
2018-05-07 07:41:05 -0400

    Update import-data.php

--- import-data.php
+++ import-data.php
@@ -12,8 +12,11 @@
     include_once "dbcontroller.php";
 	  $reader = \PhpOffice\PhpSpreadsheet\IOFactory::createReader('Xlsx');
 		$reader->setReadDataOnly(TRUE); 
+	
+    
     $fileName = $_POST['importFile']; //Name of the file chosen by the user on allCadetView.php
-		$spreadsheet = $reader->load($fileName);
+    move_uploaded_files($_FILES['importFile']['tmp_name'], $filename); //Move file to the database
+    $spreadsheet = $reader->load($fileName);
     $sheet = $spreadsheet->getActiveSheet();
     $db = new DBController();
 
@@ -30,51 +33,51 @@
       $rows[] = $cells; //Makes an array of entries that holds an array of fields for each record
     }
     $size = count($rows);
-    for($int = 1; $int < $size; $int++) //Sets each field equal to an easy to read variable
+    for($index = 1; $index < $size; $index++) //Sets each field equal to an easy to read variable
     {
-      $fName = $rows[$int][0];
-      $mName = $rows[$int][1];
-      $lName = $rows[$int][2];
-      $gender = $rows[$int][3];
-      $ssn = $rows[$int][4];
-      $genQual = $rows[$int][5];
-      $birthday = $rows[$int][6]; 
-      $race = $rows[$int][7]; 
-      $isHispanic = $rows[$int][8];
-      $email = $rows[$int][9];
-      $mStreet = $rows[$int][10]; 
-      $mStreet2 = $rows[$int][11];
-      $mCity = $rows[$int][12]; 
-      $mState = $rows[$int][13];
-      $mZip = $rows[$int][14];
-      $pStreet = $rows[$int][15];
-      $pStreet2 = $rows[$int][16]; 
-      $pCity = $rows[$int][17]; 
-      $pState = $rows[$int][18];
-      $pZip = $rows[$int][19];
-      $isCitizen = $rows[$int][20]; 
-      $ged = $rows[$int][21]; 
-      $volunteer = $rows[$int][22]; 
-      $admissionStatus = $rows[$int][23]; 
-      $schoolWithdrawDate = $rows[$int][24];
-      $unemployed = $rows[$int][25]; 
-      $underemployed = $rows[$int][26];
-      $workplace = $rows[$int][27];
-      $wage = $rows[$int][28]; 
-      $hoursWorking =  $rows[$int][29];
-      $accomplish1 = $rows[$int][30]; 
-      $accomplish2 = $rows[$int][31];
-      $recBy = $rows[$int][31]; 
-      $recNum = $rows[$int][32]; 
-      $gradeCompleted = $rows[$int][33]; 
-      $hairColor = $rows[$int][34]; 
-      $eyeColor = $rows[$int][35]; 
-      $height = $rows[$int][36]; 
-      $weight = $rows[$int][37]; 
-      $personsInHouse = $rows[$int][38]; 
-      $houseIncome = $rows[$int][39];
-      $gaResident = $rows[$int][40]; 
-      $preferredComm = $rows[$int][41];
+      $fName = $rows[$index][0];
+      $mName = $rows[$index][1];
+      $lName = $rows[$index][2];
+      $gender = $rows[$index][3];
+      $ssn = $rows[$index][4];
+      $genQual = $rows[$index][5];
+      $birthday = $rows[$index][6]; 
+      $race = $rows[$index][7]; 
+      $isHispanic = $rows[$index][8];
+      $email = $rows[$index][9];
+      $mStreet = $rows[$index][10]; 
+      $mStreet2 = $rows[$index][11];
+      $mCity = $rows[$index][12]; 
+      $mState = $rows[$index][13];
+      $mZip = $rows[$index][14];
+      $pStreet = $rows[$index][15];
+      $pStreet2 = $rows[$index][16]; 
+      $pCity = $rows[$index][17]; 
+      $pState = $rows[$index][18];
+      $pZip = $rows[$index][19];
+      $isCitizen = $rows[$index][20]; 
+      $ged = $rows[$index][21]; 
+      $volunteer = $rows[$index][22]; 
+      $admissionStatus = $rows[$index][23]; 
+      $schoolWithdrawDate = $rows[$index][24];
+      $unemployed = $rows[$index][25]; 
+      $underemployed = $rows[$index][26];
+      $workplace = $rows[$index][27];
+      $wage = $rows[$index][28]; 
+      $hoursWorking =  $rows[$index][29];
+      $accomplish1 = $rows[$index][30]; 
+      $accomplish2 = $rows[$index][31];
+      $recBy = $rows[$index][31]; 
+      $recNum = $rows[$index][32]; 
+      $gradeCompleted = $rows[$index][33]; 
+      $hairColor = $rows[$index][34]; 
+      $eyeColor = $rows[$index][35]; 
+      $height = $rows[$index][36]; 
+      $weight = $rows[$index][37]; 
+      $personsInHouse = $rows[$index][38]; 
+      $houseIncome = $rows[$index][39];
+      $gaResident = $rows[$index][40]; 
+      $preferredComm = $rows[$index][41];
       $db->createRecord("INSERT INTO cadets (fName, mName, lName, gender, ssn, genQual, birthday, race, isHispanic, email, mStreet, mStreet2, City, mState,  mZip,  pStreet, pStreet2, pCity, pState, pZip, isCitizen, ged, volunteer, admissionStatus, schoolWithdrawDate, unemployed, underemployed, workplace, wage, hoursWorking, accomplish1, accomplish2, recBy, recNum,  gradeCompleted, hairColor, eyeColor, height, weight, personsInHouse, houseIncome, gaResident, preferredComm, campusLocation
 ) VALUES ('$fName', '$mName', '$lName', '$gender', '$ssn', '$genQual', '$birthday', '$race', '$isHispanic', '$email', '$mStreet', '$mStreet2', '$City', '$mState',  '$mZip',  '$pStreet', '$pStreet2', '$pCity', '$pState', '$pZip', '$isCitizen', '$ged', '$volunteer', '$admissionStatus', '$schoolWithdrawDate', '$unemployed', '$underemployed', '$workplace', '$wage', '$hoursWorking', '$accomplish1', '$accomplish2', '$recBy', '$recNum',  '$gradeCompleted', '$hairColor', '$eyeColor', '$height', '$weight', '$personsInHouse', '$houseIncome', '$gaResident', '$preferredComm', '$campusLocation')"); //Pushes each record
     }
